2fc9c7bd7d44ad66e3d73495cd3901cc5e8f9b86
Jin-devST <jindevst@gmail.com>
2025-04-23 22:56:31 +0900

    feat: lombok 제거

--- src/main/kotlin/com/jindevst/springbootcleanarchboilterplate/infrastructure/framework/data/dto/response/BaseResponseDto.kt
+++ src/main/kotlin/com/jindevst/springbootcleanarchboilterplate/infrastructure/framework/data/dto/response/BaseResponseDto.kt
@@ -1,6 +1,5 @@
 package com.jindevst.springbootcleanarchboilterplate.infrastructure.framework.data.dto.response
 
-@Builder
 open class BaseResponseDto {
     var code: Int = 200
     var message: String = ""
